b740974c04a823278ab8e0d214ab4df3678d5a5f
KevinKVJ <jiakaiwen5216@gmail.com>
2022-07-13 21:24:32 -0600

    Start to pack the swiper component

--- Frontend_Client/src/components/Swiper/Swiper.tsx
+++ Frontend_Client/src/components/Swiper/Swiper.tsx
@@ -1,44 +1,27 @@
-import { useMemo } from 'react';
-import Slider from 'react-slick';
+import { PropsWithChildren, useMemo } from 'react';
+import Slider, { Settings } from 'react-slick';
 import 'slick-carousel/slick/slick-theme.scss';
 import 'slick-carousel/slick/slick.scss';
 
 /* Really appreciate the react-slick dev group */
+interface SwiperProps extends PropsWithChildren<Settings> {}
 
-const Swiper = () => {
+const Swiper: React.FC<SwiperProps> = ({ children, dots = true, speed = 500, ...swiperSettings }) => {
     const settings = useMemo(
         () => ({
-            dots: true,
-            infinite: true,
-            speed: 500,
-            slidesToShow: 3,
-            slidesToScroll: 3,
+            dots,
+            speed,
+            // infinite: true,
+            // slidesToShow: 3,
+            // slidesToScroll: 3,
+            ...swiperSettings,
         }),
-        []
+        [swiperSettings]
     );
     return (
         <div>
             {/* <h2> Single Item</h2> */}
-            <Slider {...settings}>
-                <div>
-                    <h3>1</h3>
-                </div>
-                <div>
-                    <h3>2</h3>
-                </div>
-                <div>
-                    <h3>3</h3>
-                </div>
-                <div>
-                    <h3>4</h3>
-                </div>
-                <div>
-                    <h3>5</h3>
-                </div>
-                <div>
-                    <h3>6</h3>
-                </div>
-            </Slider>
+            <Slider {...settings}>{children}</Slider>
         </div>
     );
 };
