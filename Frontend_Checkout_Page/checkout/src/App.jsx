efb560e9e446c9cd58fe7db7a670eace3b846770
wel992 <lwywel992@gmail.com>
2023-01-29 17:03:48 -0600

    beautify details inclding color font and display

--- Frontend_Checkout_Page/checkout/src/App.jsx
+++ Frontend_Checkout_Page/checkout/src/App.jsx
@@ -22,43 +22,47 @@ function App() {
       <div className='background'>
 
         <div className='title'>
-          baga
+          <div className="">back</div>
         </div>
+
+
         <div className='content'>
 
           <div className='checkout'>Checkout</div>
-
+          <div className='horizontal-ruler'></div>
           <div className='block'>
             <div className='small-title'>Address</div>
-            <div className='payment-method'>
+            <div className='address-display'>
               <div>
                 <SvgIcon name='mcpic' width={100} height={100} color='#646cff' />
 
               </div>
 
               <div>
-                <div className='address'>Office</div>
-                <div className='address'>Adi Sucipto St. No.23, Solo,Central Java, Indonesia</div>
+                <div className='address-line1'>Office</div>
+                <div className='address-line2'>Adi Sucipto St. No.23, Solo,Central Java, Indonesia</div>
               </div>
 
             </div>
           </div>
 
+          <div className='horizontal-ruler'></div>
           <div className='block'>
             <div className='small-title'>Payment Method</div>
-            <div className='payment-method'>
+            <div className='mastercard'>
               <div>
                 <SvgIcon name='master' width={100} height={100} color='#646cff' />
 
               </div>
 
-              <div>
+              <div className='master-text'>
                 <div>Master Card</div>
                 <div>********7896</div>
               </div>
             </div>
           </div>
 
+          <div className='horizontal-ruler'></div>
           <div className='block'>
             <div className='wrapper'>
               <div className='sum'>Shipping cost</div>
@@ -70,15 +74,17 @@ function App() {
               <div className='amount'>3.70</div>
             </div>
 
+            <div className='dashed-ruler'></div>
             <div className='wrapper'>
               <div className='sum'>Total</div>
               <div className='amount'>39.70</div>
             </div>
           </div>
 
+          <div className='horizontal-ruler'></div>
           <div className='block'>
             <div className='small-title'>Add Voucher or Promo Code</div>
-            <div className='voucher-box'> add voucher or promo code here</div>
+            <div className='voucher-box'> <div className='promo-txt'>Add voucher or promo code here</div></div>
           </div>
 
 
