4ccccab0d2b714f2ac6e42e22aa88f8707460859
KevinKVJ <jiakaiwen5216@gmail.com>
2023-03-31 15:20:09 -0600

    Removed the Backend dir because it is an out of date backend project

--- Backend/src/main/java/app/ordering/food/mapper/MerchantMapper.java
+++
@@ -1,9 +0,0 @@
-package app.ordering.food.mapper;
-
-import app.ordering.food.entity.Merchant;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MerchantMapper extends BaseMapper<Merchant> {
-}
--- Backend/src/main/java/app/ordering/food/service/MerchantService.java
+++
@@ -1,7 +0,0 @@
-package app.ordering.food.service;
-
-import app.ordering.food.entity.Merchant;
-import com.baomidou.mybatisplus.extension.service.IService;
-
-public interface MerchantService extends IService<Merchant> {
-}
